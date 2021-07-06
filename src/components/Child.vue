8e2469cc5637c15c403d145516af545b76390cee
Carlos Pedro <carlospedr@gmail.com>
2021-07-06 22:59:07 +0200

    toRef makes me sad

---
+++ src/components/Child.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <label for="componentInput">Input in Component</label>
+    <input id="componentInput" type="text" v-model="watchedMsg"/> 
+    <br>
+    <label for="componentSpan"></label> <span id="componentSpan">{{ watchedMsg }}</span>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent, toRefs, watch} from 'vue';
+
+export default defineComponent({
+  props: {
+    msg: {
+      type: String,
+      required: true
+    },
+  },
+  setup(props) {
+    const {msg: watchedMsg} = toRefs(props);
+
+    //watch(watchedMsg, (v)=> console.log(`New message value ${watchedMsg}`))
+
+    return {watchedMsg};
+
+  }
+});
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <input type="text" v-model="message"/>
+    <Child :msg="message"/>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent, ref, watch} from 'vue';
+import Child from "@/components/Child.vue";
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    Child,
+  },
+  setup() {
+    const message = ref("Original Text");
+    watch(message, (v) => console.log("Parent view message", v))
+    return {message};
+  }
+});
+</script>
